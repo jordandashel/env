177de10c4c541260b58996fd64abdec29d9dd1a7
Jordan Dashel <jordan.dashel@gmail.com>
2015-09-14 23:29:31 -0700

    update

--- no_place_like_home.sh
+++ no_place_like_home.sh
@@ -9,42 +9,12 @@ if [[ $UID != 0 ]]; then
 fi
 
 apt-get install build-essential
-apt-get install libevent-dev libncurses-dev curl zsh
-
-sh $OG_DIR/pl_fonts/install.sh
-
-cd ~/
-if [ -e tmux-2.0.tar.gz ]; then
-  rm ~/tmux-2.0.tar.gz
-fi
-if [ -d tmux-2.0 ]; then 
-  rm -rf ~/tmux-2.0
-fi
-wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
-tar -zxvf tmux-2.0.tar.gz
-cd tmux-2.0
-./configure
-make
-make install
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
-vim +PluginInstall +qall
-
-if [ ! -d ~/.config/base16-shell ]; then
-  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-fi
-
-curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh > ./zsh_install.sh
-sh ./zsh_install.sh
-chsh -s /bin/zsh
+apt-get install libevent-dev libncurses-dev 
+# my favorite/essential utilities
+apt-get install curl zsh vim pandoc
 
 if [ -e ~/.tmux.conf ]; then
   rm ~/.tmux.conf
-fi
-if [ -e ~/.zshrc ]; then
-  rm ~/.zshrc
 fi
 if [ -e ~/.vimrc ]; then 
   rm ~/.vimrc
@@ -57,5 +27,36 @@ ln -s $OG_DIR/.tmux.conf ~/.tmux.conf
 ln -s $OG_DIR/.zshrc ~/.zshrc
 ln -s $OG_DIR/.vimrc ~/.vimrc
 ln -s $OG_DIR/.bashrc ~/.bashrc
+sh $OG_DIR/pl_fonts/install.sh
+
+cd ~/
+if [ -e tmux-2.0.tar.gz ]; then
+  rm ~/tmux-2.0.tar.gz
+fi
+if [ -d tmux-2.0 ]; then 
+  rm -rf ~/tmux-2.0
+fi
+wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz
+tar -zxvf tmux-2.0.tar.gz
+cd tmux-2.0 && \
+./configure && \
+make && \
+make install
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+vim +PluginInstall +qall
+
+if [ ! -d ~/.config/base16-shell ]; then
+  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
+
+curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh > ~/.zsh_install.sh
+sh ~/.zsh_install.sh
+
+if [ -e ~/.zshrc ]; then
+  rm ~/.zshrc
+fi
 
 cd $OG_DIR
