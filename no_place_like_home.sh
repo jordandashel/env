f7ea13612c65d0d8f0a04eada9d1bfcc0fe0c0f6
jordan.dashel@gmail.com <jordan.dashel@gmail.com>
2015-09-17 11:30:04 -0700

    separated into functions

--- no_place_like_home.sh
+++ no_place_like_home.sh
@@ -3,66 +3,95 @@
 OG_DIR=$PWD
 
 if [ "$(whoami)" != "root" ]; then
-  echo "sudo required:"
-  echo "sudo $0 $*"
-  exit 1
+	echo "sudo required:"
+	echo "sudo $0 $*"
+	exit 1
 fi
 
-apt-get install --yes build-essential
-apt-get install --yes libevent-dev libncurses-dev 
-# my favorite/essential utilities
-apt-get install --yes curl zsh vim pandoc
+# -- FUNCTIONS -- #
 
-# -- SETTINGS --
+apt_install_programs(){
+	apt-get install --yes build-essential
+	apt-get install --yes libevent-dev libncurses-dev 
+	# my favorite/essential utilities
+	apt-get install --yes curl zsh vim pandoc
+}
 
-git config merge.tool vimdiff
-git config merge.conflictstyle diff3
-git config mergetool.prompt false
+configure_git(){
+	git config merge.tool vimdiff
+	git config merge.conflictstyle diff3
+	git config mergetool.prompt false
+}
 
-if [ -e $HOME/.tmux.conf ]; then
-  rm $HOME/.tmux.conf
-  ln -s $OG_DIR/.tmux.conf $HOME/.tmux.conf
-fi
-if [ -e $HOME/.vimrc ]; then 
-  rm $HOME/.vimrc
-  ln -s $OG_DIR/.vimrc $HOME/.vimrc
-fi
-if [ -e $HOME/.bashrc ]; then 
-  rm $HOME/.bashrc
-  ln -s $OG_DIR/.bashrc $HOME/.bashrc
-fi
 
-sh $OG_DIR/pl_fonts/install.sh
+install_fonts(){
+	sh $OG_DIR/pl_fonts/install.sh
+}
 
-cd $HOME/
-if [ -e tmux-2.0.tar.gz ]; then
-  rm $HOME/tmux-2.0.tar.gz
-fi
-if [ -d tmux-2.0 ]; then 
-  rm -rf $HOME/tmux-2.0
-fi
-wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz && \
-tar -zxvf tmux-2.0.tar.gz && \
-cd tmux-2.0 && \
-./configure && \
-make && \
-make install
+install_tmux(){
+	cd $HOME/
 
-if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
-  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-fi
-vim +PluginInstall +qall
+	if [ -e tmux-2.0.tar.gz ]; then
+		rm $HOME/tmux-2.0.tar.gz
+	fi
+	if [ -d tmux-2.0 ]; then 
+		rm -rf $HOME/tmux-2.0
+	fi
+	wget https://github.com/tmux/tmux/releases/download/2.0/tmux-2.0.tar.gz && \
+	tar -zxvf tmux-2.0.tar.gz && \
+	cd tmux-2.0 && \
+	./configure && \
+	make && \
+	make install
 
-if [ ! -d $HOME/.config/base16-shell ]; then
-  git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
-fi
+	cd $OG_DIR
+}
 
-curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh > $HOME/zsh_install.sh
-sh $HOME/zsh_install.sh
+install_vundle_and_vim_plugins(){
+	if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+		git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+	fi
+	vim +PluginInstall +qall
+}
 
-if [ -e $HOME/.zshrc ]; then
-  rm $HOME/.zshrc
-  ln -s $OG_DIR/.zshrc $HOME/.zshrc
-fi
+install_base16_shell(){
+	if [ ! -d $HOME/.config/base16-shell ]; then
+		git clone https://github.com/chriskempson/base16-shell.git $HOME/.config/base16-shell
+	fi
+}
 
-cd $OG_DIR
+install_oh_my_zsh(){
+	curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh > $HOME/zsh_install.sh
+	sh $HOME/zsh_install.sh
+
+}
+
+link_dot_files(){
+	if [ -e $HOME/.tmux.conf ]; then
+		rm $HOME/.tmux.conf
+		ln -s $OG_DIR/.tmux.conf $HOME/.tmux.conf
+	fi
+	if [ -e $HOME/.vimrc ]; then 
+		rm $HOME/.vimrc
+		ln -s $OG_DIR/.vimrc $HOME/.vimrc
+	fi
+	if [ -e $HOME/.bashrc ]; then 
+		rm $HOME/.bashrc
+		ln -s $OG_DIR/.bashrc $HOME/.bashrc
+	fi
+	if [ -e $HOME/.zshrc ]; then
+		rm $HOME/.zshrc
+		ln -s $OG_DIR/.zshrc $HOME/.zshrc
+	fi
+}
+
+# configure
+
+apt_install_programs
+configure_git
+install_fonts
+install_tmux
+install_vundle_and_vim_plugins
+install _base16_shell
+install_oh_my_zsh
+link_dot_files #must follow oh-my-zsh install or .zshrc will be clobbered 
