c65868862b6adc8d15b3f87aa7a3b51617880adf
Jordan Dashel <jordan.dashel@gmail.com>
2016-07-28 10:11:41 -0700

    delete a bunch of themes

--- .emacs.d/elpa/dracula-theme-20160228.1913/dracula-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dracula-theme" "20160228.1913" "Dracula Theme" '((emacs "24")) :url "https://github.com/zenorocha/dracula-theme")
--- .emacs.d/elpa/mustard-theme-20141115.2302/mustard-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "mustard-theme" "20141115.2302" "an Emacs 24 theme based on Mustard (tmTheme)" '((emacs "24.0")) :url "https://github.com/emacsfodder/tmtheme-to-deftheme")
